858a4b2b1433410fdc41d533c090e2b519af25b8
onomatic <bifunctorial@gmail.com>
2018-02-02 20:15:53 +1100

    added SP.thy

---
+++ Hoare_Sep_Tactics/SP.thy
@@ -0,0 +1,15 @@
+theory SP
+imports
+"~~/src/HOL/Main"
+"~~/src/HOL/Eisbach/Eisbach"
+begin
+
+text \<open>Rule collections for strongest postconditions\<close>
+named_theorems sp
+named_theorems sp_pre
+
+text {* Strongest postcondition method setup *}
+method sp declares sp = ((((rule sp)+), (rule sp_pre, rule sp, assumption?)?)  |
+                        (rule sp_pre, rule sp, assumption?))
+
+end
